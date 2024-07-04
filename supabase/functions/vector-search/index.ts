fb90a7219438a69ae9488addcb9e5eb90e8d336f
satwik <satwikloka1@gmail.com>
2024-07-04 09:36:34 +0000

    Authourization null

--- supabase/functions/vector-search/index.ts
+++ supabase/functions/vector-search/index.ts
@@ -24,7 +24,7 @@ export class ApplicationError extends Error {
 export class UserError extends ApplicationError {}
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'x-client-info, apikey, content-type',
 }
 
 serve(async (req) => {
